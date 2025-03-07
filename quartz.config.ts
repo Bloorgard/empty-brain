3f9a1a94e69e59a3bf5a1ac1153e3e08e48299d5
Peter Pustota <peter@pustota.name>
2025-03-07 23:14:08 +0500

    SPA false

--- quartz.config.ts
+++ quartz.config.ts
@@ -10,7 +10,7 @@ const config: QuartzConfig = {
   configuration: {
     pageTitle: "Empty Brain",
     pageTitleSuffix: "",
-    enableSPA: true,
+    enableSPA: false,
     enablePopovers: true,
     analytics: {
       provider: "plausible",
